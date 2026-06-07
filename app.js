b4340d933a0ba9b0493ce33e37be6bf2033810c0
rohit <rohitdeppi38@gmail.com>
2026-06-07 16:49:19 +0530

    with public files

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set('view engine','ejs');
+app.set(express.static(path.join(__dirname,'public')))
 
 app.get('/',function(req,res){
     res.send("HEY");
 })
 
-app.listen(3000,function(){
+server.listen(3000,function(){
     console.log("server started on PORT 3000");
 })
