f0e61db6613c5d324e959410eb22a05cf8b1ae01
rohit <rohitdeppi38@gmail.com>
2026-06-14 21:44:09 +0530

    completing the project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const path = require('path')
 const socketio = require('socket.io');
 
 const app = express();
@@ -10,8 +11,20 @@ const io = socketio(server);
 app.set('view engine','ejs');
 app.set(express.static(path.join(__dirname,'public')))
 
+
+io.on("connection",function(socket){
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id:socket.id,...data});
+    });
+
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected",socket.id);
+    })
+})
+
+
 app.get('/',function(req,res){
-    res.send("HEY");
+    res.render("index")
 })
 
 server.listen(3000,function(){
