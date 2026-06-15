be2094c82758df6a372aa4bfaeb1d4cbb79acb44
rohit <rohitdeppi38@gmail.com>
2026-06-15 20:06:38 +0530

    modifiying the files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set('view engine','ejs');
-app.set(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')))
 
 
 io.on("connection",function(socket){
@@ -27,6 +27,22 @@ app.get('/',function(req,res){
     res.render("index")
 })
 
-server.listen(3000,function(){
-    console.log("server started on PORT 3000");
-})
+const DEFAULT_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+function startServer(port){
+    server.listen(port, function(){
+        console.log(`server started on PORT ${port}`);
+    });
+
+    server.once('error', function(err){
+        if(err && err.code === 'EADDRINUSE'){
+            console.warn(`Port ${port} in use, trying ${port + 1}...`);
+            startServer(port + 1);
+        } else {
+            console.error(err);
+            process.exit(1);
+        }
+    });
+}
+
+startServer(DEFAULT_PORT);
--- public/js/script.js
+++ public/js/script.js
@@ -2,41 +2,61 @@ const socket = io();
 
 if(navigator.geolocation){
     navigator.geolocation.watchPosition((position)=>{
+        console.log(position);
         const {latitude,longitude}= position.coords;
         socket.emit("send-location",{latitude,longitude});
-    },(error)=>{
-        console.error(error);
-    },
+    },(error) => {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            console.error("User denied location permission");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.error("GPS signal unavailable");  // likely your case
+            break;
+        case error.TIMEOUT:
+            console.error("GPS timed out");           // also possible
+            break;
+    }
+},
     {
         enableHighAccuracy:true,
-        timeout:5000,
+        timeout:30000,
         maximumAge:0,
     }
 )
 }
 
-const map = L.map("map").setView([0,0],10);
+const map = L.map("map").setView([0,0],4);
 
-L.tileLayer("https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png",{
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution:"OpenStreetMap"
 }).addTo(map)
 
 const markers = {};
 
-socket.on("receive-location",(data)=>{
-    const {id,latitude,longitude} = data;
-    map.setView([latitude,longitude],16); 
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
 
-    if(markers[id]){
-        markers[id].setLatLng([latitude,longitude]);
-    }else{
-        markers[id] = L.marker([latitude,longitude]).addTo(map);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
-})
 
-socket.on("user-disconnect",(id)=>{
+    // Fit bounds to show every marker
+    const allMarkers = Object.values(markers);
+    if (allMarkers.length > 1) {
+        const group = L.featureGroup(allMarkers);
+        map.fitBounds(group.getBounds().pad(0.2));
+    } else {
+        map.setView([latitude, longitude], 16);
+    }
+});
+
+socket.on("user-disconnected",(id)=>{
     if(markers[id]){
         map.removeLayer(markers[id]);
         delete markers[id];
     }
 });
+
